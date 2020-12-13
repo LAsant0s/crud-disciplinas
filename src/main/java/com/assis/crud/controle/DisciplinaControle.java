579e076f0c0217e034a417a4702741bf2d421126
Luan Assis <luan.assis05.santos@gmail.com>
2020-12-12 21:15:59 -0300

    Exercicio 9 finalizado - novo registro

--- src/main/java/com/assis/crud/controle/DisciplinaControle.java
+++ src/main/java/com/assis/crud/controle/DisciplinaControle.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.assis.crud.dao.DisciplinaDAO;
 import com.assis.crud.dominio.Disciplina;
@@ -18,6 +19,23 @@ public class DisciplinaControle {
 		List<Disciplina> lista = dao.todos();
 		modelo.addAttribute("lista",lista);
 		return "disciplinas"; 
+	}
+	@GetMapping("/")
+	public String home() {
+		return "home"; 
+	}
+	
+	@GetMapping("/cadastrar-disciplina")
+	public String exibeForm(Model modelo) {
+		modelo.addAttribute("disciplina", new Disciplina());
+		return "form-disciplina";
+	}
+	
+	@PostMapping("/cadastrar-disciplina")
+	public String processaForm(Disciplina disciplina) {
+		DisciplinaDAO dao = new DisciplinaDAO(); 
+		dao.inserir(disciplina);
+		return "redirect:/disciplinas";
 	}
 	
 }
